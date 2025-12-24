dda6144847188f2663c705a277ddb62eb6ecb0eb
IslamOsmanov <islamosman20061300@gmail.com>
2025-12-24 04:26:51 +0400

    Add project files.

---
+++ Pronia/Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pronia.Models;
+
+public class Card
+{   
+    public int Id { get; set; }
+    [MinLength(5,ErrorMessage ="Min length: 5")]
+    public string Title { get; set; } = null!;
+    [Required(ErrorMessage ="Field can't be empty!")]
+    public string Description { get; set; } = null!;
+    public string ImagePath { get; set; } = null!;
+
+
+}
